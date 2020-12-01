00d7575f6769fd37e459783eaab3123a28c8bf37
aidem <956534763@qq.com>
2020-12-01 16:44:20 +0800

    完善searchDemo

--- src/main/java/com/aidem/search/SearchDemo.java
+++ src/main/java/com/aidem/search/SearchDemo.java
@@ -11,18 +11,21 @@ import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.*;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryRequest;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -378,30 +381,73 @@ public class SearchDemo {
         }
     }
 
-
     /**
-     * 根据regexp查询
+     * 深分页 scroll
      */
     @Test
-    public  void findByRegexp() throws IOException {
-        //  创建request对象
-        SearchRequest request = new SearchRequest(index);
-        request.types(type);
-        //  指定查询条件
+    public void scrollSearch() throws IOException {
+        // 1.创建request
+        SearchRequest searchRequest = new SearchRequest(index);
+        searchRequest.types(type);
+        //  2.指定scroll信息,过期时间
+        searchRequest.scroll(TimeValue.timeValueMinutes(1L));
+        //  3.指定查询条件
         SearchSourceBuilder builder = new SearchSourceBuilder();
-        //--------------------------------------------------
-        builder.query(QueryBuilders.regexpQuery("mobile","138[0-9]{8}"));
-        //------------------------------------------------------
-        request.source(builder);
-        // 执行
-        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
-        // 输出结果
+        builder.size(4);
+        builder.sort("fee", SortOrder.DESC);
+        searchRequest.source(builder);
+        // 4.获取返回结果scrollId,获取source
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        String scrollId = response.getScrollId();
+        System.out.println("-------------首页数据---------------------");
         for (SearchHit hit : response.getHits().getHits()) {
             System.out.println(hit.getSourceAsMap());
         }
+        while (true) {
+            // 5.创建scroll request
+            SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+            // 6.指定scroll 有效时间
+            scrollRequest.scroll(TimeValue.timeValueMinutes(1L));
+            // 7.执行查询，返回查询结果
+            SearchResponse scroll = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+            // 8.判断是否查询到数据，查询到输出
+            SearchHit[] searchHits = scroll.getHits().getHits();
+            if (searchHits != null && searchHits.length > 0) {
+                System.out.println("-------------下一页数据---------------------");
+                for (SearchHit hit : searchHits) {
+                    System.out.println(hit.getSourceAsMap());
+                }
+            } else {
+                //  9.没有数据，结束
+                System.out.println("-------------结束---------------------");
+                break;
+            }
+        }
+        // 10.创建 clearScrollRequest
+        ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+        // 11.指定scrollId
+        clearScrollRequest.addScrollId(scrollId);
+        //12.删除scroll
+        ClearScrollResponse clearScrollResponse = client.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+        // 13.输出结果
+        System.out.println("删除scroll:" + clearScrollResponse.isSucceeded());
     }
 
-
+    /**
+     * deleteByQuery， 根据条件删除
+     * 若删除多的话，建议将数据查询出来，插入到新的index里，效率会高点
+     */
+    public void deleteByQuery() throws IOException {
+        // 1.创建DeleteByQueryRequest
+        DeleteByQueryRequest request = new DeleteByQueryRequest(index);
+        request.types(type);
+        // 2.指定条件
+        request.setQuery(QueryBuilders.rangeQuery("fee").lt(20));
+        // 3.执行
+        BulkByScrollResponse response = client.deleteByQuery(request, RequestOptions.DEFAULT);
+        // 4.输出返回结果
+        System.out.println(response.toString());
+    }
 
 
 
